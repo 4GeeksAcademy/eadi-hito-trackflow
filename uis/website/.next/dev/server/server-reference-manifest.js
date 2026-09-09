956332ce3cb8ee4b955e07a2d9f35259d1f8d3a8
Evelin Duarte <evelindfodon@gmail.com>
2026-09-09 22:41:08 +0000

    Ingeniería impulsada por IA

---
+++ uis/website/.next/dev/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"o6QCbK6f82PjrIsFmFYSczYYSOoWVxcXY7npKwZmB8k=\"\n}"
---
+++ uis/website/.next/server/chunks/ssr/01vi_next_dist_client_components_builtin_forbidden_0g51tlb.js
@@ -0,0 +1,3 @@
+module.exports=[20174,(a,b,c)=>{"use strict";Object.defineProperty(c,"__esModule",{value:!0}),Object.defineProperty(c,"default",{enumerable:!0,get:function(){return f}});let d=a.r(22615),e=a.r(32225);function f(){return(0,d.jsx)(e.HTTPAccessErrorFallback,{status:403,message:"This page could not be accessed."})}("function"==typeof c.default||"object"==typeof c.default&&null!==c.default)&&void 0===c.default.__esModule&&(Object.defineProperty(c.default,"__esModule",{value:!0}),Object.assign(c.default,c),b.exports=c.default)},35602,function(a){a.n(a.i(20174))}];
+
+//# sourceMappingURL=01vi_next_dist_client_components_builtin_forbidden_0g51tlb.js.map
